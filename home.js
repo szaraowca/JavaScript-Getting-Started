6b439e025943a1e30ed5ba3489c46ae1feb63f0b
Klaudia <klaudiuszka1@gmail.com>
2020-08-28 20:23:09 +0200

    first variables

---
+++ home.js
@@ -0,0 +1,16 @@
+let name ="Hello "World"",
+    price =20.22,
+    discounted = false;
+
+    price= price + 1;
+
+    /* you can use also - * + %, and you can write down: price *=2, 
+    */ 
+const sale = 10.22;
+
+// below choose between name, price, discounted or sale
+
+showMessage(name)
+
+//if you write ++price (--price) it will add (subtract)  1 to price, -
+
