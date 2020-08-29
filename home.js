9ca3321b727cf5a03f994c49ad353b6753c409f5
Klaudia <klaudiuszka1@gmail.com>
2020-08-29 16:43:20 +0200

    some if operations

--- home.js
+++ home.js
@@ -1,7 +1,19 @@
-let person = {
-    firstName: 'John',
-    lastName: 'Adams'
-};
+let price=20;
 
+if(price == 19) {
+    showMessage('price is 20');}
 
-showMessage(person.firstName + " " + person.lastName);
+else if (price > 10) {
+    showMessage ("under 10")}
+
+// (price > 10) ? showMessage('under 10') : showMessage('10')
+
+//Truthy is everything not falsy, but "0" is true!!
+
+/*if (+(1.1+1.3).toFixed(2) === 2.4) {
+    let message = "hello";
+    showMessage(message);
+}
+ + here changes string into number, ! before == means not equal
+*/
+
