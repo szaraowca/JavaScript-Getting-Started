8dcfbb9a968fddbfad0de4c5b02ee2bf7aa0fe52
Klaudia <klaudiuszka1@gmail.com>
2020-09-17 16:28:41 +0200

    object methods'

--- home.js
+++ home.js
@@ -1,15 +1,13 @@
 
-let mySymbol = Symbol();
+
 let person = {
     name: 'John',
     age: 32,
     partTime: false,
-    [mySymbol]: 'secretInformation'
+    showInfo : function(realAge) {
+        showMessage(this.name + ' is '+ realAge);
+    }
 
 };
 
-person.age = 42;
-person ['partTime']= true;
-showMessage (person.partTime);
-
-//if we have a secret information, we can "hide" it inside object
+person.showInfo(42);
