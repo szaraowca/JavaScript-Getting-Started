0f34b0e120795469c2cc344175c3fd89b18a7848
Klaudia <klaudiuszka1@gmail.com>
2020-09-17 15:53:12 +0200

    object properties

--- home.js
+++ home.js
@@ -1,31 +1,15 @@
-//Loops- examples:
 
-//first "for" loop: in console should be: numbers 0 to 4, 
+let mySymbol = Symbol();
+let person = {
+    name: 'John',
+    age: 32,
+    partTime: false,
+    [mySymbol]: 'secretInformation'
 
-for (let i=0; i<5; i++) {
-    console.log (i);
-}
+};
 
- //second "while" loop: should be the same numbers
+person.age = 42;
+person ['partTime']= true;
+showMessage (person.partTime);
 
-let count = 1;
-while (count <5) {
-
-console.log(count); count ++;
-}
-
-//another example "while" loop: in console will be numbers 3, 2, 1 
-
-let z = 3; 
-while (z > 0) {
-    console.log(z);
-    z--;
-}
-
-//last "do" loop, similar to the while loop, in console should be numbers from 6 to 1
-
-let x = 6;
-do {
-    console.log(x);
-    x--;
-} while (x> 0);
+//if we have a secret information, we can "hide" it inside object
